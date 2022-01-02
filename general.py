ef211c39e78630c4456cd8a539023b9ddde2ad16
warren jay <40836157+warrenjayv@users.noreply.github.com>
2022-01-01 19:23:56 -0500

    Add files via upload

--- general.py
+++ general.py
@@ -3,9 +3,12 @@ import discord
 
 class general:
     #~properties
-    usr1 = [0] * 0x5
-    usr2 = [0] * 0x5
+    usr1 = [0] * 0x6
+    usr2 = [0] * 0x6
     
+    health1 = 0x3E8
+    health2 = 0x3E8 
+
     #~units
     trp = [ '░', '░' ]
     
@@ -13,11 +16,11 @@ class general:
     acres = 0x8c       #plot size
     plot = [0] * acres #plot
     area1 = 0x0        #player 1 area
-    area2 = 0x78       #player 2 area 
-   # fma = 0x0        #player 1 message area
-   # bta = 0x64       #battle area
-   # sma = 0x80       #player 2 message area
-
+    area2 = 0xA        #player 2 area 
+   # fma = 0x0         #player 1 message area
+   # bta = 0x64        #battle area
+   # sma = 0x80        #player 2 message area
+ 
     #~vertical zones
     A = 0              #0
     B = 0x14           #20
@@ -37,46 +40,67 @@ class general:
     def __init__(self):
         for i in range(self.acres):
             self.plot[i] = "_"
-            if i < 0x29:
-                self.plot[i] = '_'
             if i%0x14 == 0:
                 self.plot[i] = '\n'
 
     #~attack
-    def go_attack(self, player, i, offset):
-            self.plot[offset + i + self.D + 1] = self.trp[player]
-            self.plot[offset + i + self.E ] = self.trp[player]
-            self.plot[offset + i + self.F ] = self.trp[player]
-            self.plot[offset + i + self.G + 1] = self.trp[player]
+    def go_attack(self, i, offset):
+            self.plot[offset + i + self.D + 1] = self.trp[0]
+            self.plot[offset + i + self.E ] = self.trp[0]
+            self.plot[offset + i + self.F ] = self.trp[0]
+            self.plot[offset + i + self.G + 1] = self.trp[0]
             if i > 0:
                 self.plot[offset + i + self.D - 1 + 1] = '_'
-                self.plot[offset + i + self.D + 1] = self.trp[player]
+                self.plot[offset + i + self.D + 1] = self.trp[0]
                 self.plot[offset + i + self.E - 1] = '_'
-                self.plot[offset + i + self.E ] = self.trp[player]
+                self.plot[offset + i + self.E ] = self.trp[0]
                 self.plot[offset + i + self.F - 1] = '_'
-                self.plot[offset + i + self.F ] = self.trp[player]
+                self.plot[offset + i + self.F ] = self.trp[0]
                 self.plot[offset + i + self.G - 1 + 1] = '_'
-                self.plot[offset + i + self.G + 1] = self.trp[player]
+                self.plot[offset + i + self.G + 1] = self.trp[0]
+            
+    def go_defend(self):
 
-    def set_action(self, player):
-        j = 0
-        start = 0x7 + self.B 
+                self.plot[ 0x8 + self.D + self.area2 ] = self.trp[0]
+                self.plot[ 0x8 + self.E + self.area2 ] = self.trp[0]
+                self.plot[ 0x8 + self.F + self.area2 ] = self.trp[0]
+                self.plot[ 0x8 + self.G + self.area2 ] = self.trp[0]                
+
+    def set_action(self):
+
+        start1 = 0x7 + self.A
+        start2 = 0x7 + self.B 
         length = len(self.attack)
 
-        for i in range( start , start + length ):
-            self.plot[i] = self.attack[j] 
+        j = 0
+        for i in range( start2 , start2 + length ):
+            self.plot[i] = self.defend[j] 
             j += 1
 
+        j = 0
+        for i in range ( start1 , start1 + length ):
+            self.plot[i] = self.attack[j]
+            j += 1 
+            
+    def set_user(self, author, mention ):
+        for i in range(0x6):
+            if i == 0:
+                temp = 1
+            else:
+                temp = 0
 
-    def set_user(self, player, mention ):
-        for i in range(0x5):
+            #mentioned
             self.usr2[i] = mention[i]
-            self.plot[i + self.B ] = self.usr2[i]
+            self.plot[i + self.B + temp] = self.usr2[i]
 
-        self.set_action(player)
+            #author
+            self.usr1[i] = author[i]
+            self.plot[i + self.A + temp] = self.usr1[i]
+
+        self.set_action()
 
     def clear_users(self):
-        for i in range(0x5):
+        for i in range(0x6):
             self.usr1[i] = ' '
             self.usr2[i] = ' '
     
@@ -86,19 +110,18 @@ class general:
             if i%0x14 == 0:
                self.plot[i] = '\n'
 
-
-
 #  plot (x2)
 #       ----- zone 0 ------     ----- zone 1 ------
-#      <-----  player 1 ------>      <----- action ------> 
+#      <-----  player 1 ------>           <--- action ---->
 #  A | 0x1  0x2  0x3  0x4  0x5  0x6  0x7  0x8  0x9  0xA  | message area
 #      <-----  player 2 ------>
 #  B | 0xB  0xC  0xD  0xD  0xE  0xF  0x11 0x12 0x13 0x14 | message area
+#      <----- player 1 health --->
 #  C | 0x15 0x16 0x17 0x18 0x19 0x1A 0x1B 0x1C 0x1D 0x1E
-#              <-------- battle area -------->
+#              <-------- battle area -------->  
 #  D | 0x1F 0x20 0x21 0x22 0x23 0x24 0x25 0x26 0x27 0x28
 #  E | 0x29 0x2A 0x2B 0x2C 0x2D 0x2E 0x2F 0x30 0x31 0x32
 #  F | 0x33 0x34 0x35 0x36 0x37 0x38 0x39 0x3A 0x3B 0x3C
 #  G | 0x3D 0x3E 0x3F 0x41 0x42 0x43 0x44 0x45 0x46 0x47
 #  H | 0x48 0x49 0x4A 0x4B 0x4C 0x4D 0x4E 0x4F 0x50 0x51
-
+#       1    2     3    4   5       <-- dice -->
--- main.py
+++ main.py
@@ -9,30 +9,58 @@ TOKEN = os.getenv('TOKEN')
 
 genlt = general()
 
+
 class MyClient(discord.Client):
+  
+    #~parameters
+    storage = []
+    counter = 0
+
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
-
+ 
     async def on_message(self, message):
+    
+        #~store message
         print('Message from {0.author}: {0.content}'.format(message))
+        self.storage.append('sniped: `{0.author}`: `{0.content}`'.format(message))
+        self.counter += 1
+        
+        #~reset
+        if self.counter > 2:
+            self.storage.clear
+            self.counter = 0
+           
+        print("counter: {}".format(self.counter))
 
         if message.content.startswith('st_ about'):
             await message.channel.send("`strategybot is a turn based, player vs player ancient battle on discord!`")
 
+        if message.content.startswith('st_ snipe'):
+            await message.channel.send(self.storage[0])
+
         if message.content.startswith('st_ attack'):
+            # ~ steps ~
+            x = 0 
+
             # ~ set users ~
             mention  = message.mentions[0].display_name
-            genlt.set_user(1, mention)
+            author   = message.author.name
+            genlt.set_user(author , mention)
+            genlt.go_defend()
+         
+            for i in range(12):
+                if x < 15:
+                    genlt.go_attack(x, 1)
+                    x += 1
 
-            for i in range(10):
-                genlt.go_attack(0, i, 2)
                 land = ' '.join(str(v) for v in genlt.plot)
                 if (i < 1):
                     plot = await message.channel.send('{}'.format(land))
                 else:
                     await plot.edit(content=land)
                     
-                time.sleep(2)
+                time.sleep(1)
 
             genlt.clear_users()
             genlt.reset()
